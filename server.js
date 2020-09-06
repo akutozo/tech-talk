dd4dbbd52e0902bd68b28a7fe004917b2ede1abe
Jacob Daughtry <akutozo@gmail.com>
2020-09-06 11:36:44 -0500

    Additonal server setup

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
 
-// const routes = require('./controllers');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
 const app = express();
