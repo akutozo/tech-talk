9490f6d751c20789e3aec9f6c97bd7f258502c33
Jacob Daughtry <akutozo@gmail.com>
2020-09-06 14:05:01 -0500

    Setting up Models, Get and POST working

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,5 @@
 const router = require('express').Router();
 
-// const homeRoutes = require('./home-routes.js');
-// router.use('/', homeRoutes);
-
 const apiRoutes = require('./api');
 router.use('/api', apiRoutes);
 
---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO user (username, email, password)
+VALUES
+    ('jdaughtry', 'bigkahuna@tt.com', 'password'),
+    ('masterchef', 'guyfieriseviltwin@fn.com', 'password'),
+    ('kingjames', 'lebron@theking.com', 'password'),
+    ('nreagan', 'nreagan@trp.com', 'password'),
+    ('bababoo', 'sendhelp@thezoo.com', 'password'),
+    ('harambe', 'rip@4ever.net', 'password');
+
+
+INSERT INTO post (title, post_url, user_id)
+VALUES
+    ('Tech Talks Launched', 'www.forbes.com', '1'),
+    ('Living Forever in the Hearts', 'https://en.wikipedia.org/wiki/Killing_of_Harambe', '6'),
+    ('Flavor Town Wiki', 'https://www.mashed.com/164319/the-untold-truth-of-guy-fieri/', '2'),
